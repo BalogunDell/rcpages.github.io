ee77e958284c272dfa5a2d5d5b3d05f06e7c6e8f
Balogun Abiodun Delight <delighteddell@gmail.com>
2017-11-22 15:44:56 +0100

    initial commit

---
+++ Assets/js/index.js
@@ -0,0 +1,170 @@
+$(document).ready(() => {
+
+    // ON MOUSE ENTER
+    $('#corporates-holder').on('mouseenter',() => {
+        $('#corporates-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.5)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#corporate-text').css({
+            'font-size': '32px',
+            'transition': 'font-size 1s'
+        });
+    });
+
+    // ON MOUSE LEAVE
+    $('#corporates-holder').on('mouseleave',() => {
+        $('#corporates-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.2)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#corporate-text').css({
+            'font-size': '30px',
+            'transition': 'font-size 1s'
+        });
+    })
+
+
+    // CASUALS
+
+    // ON MOUSE ENTER
+    $('#casuals-holder').on('mouseenter',() => {
+        $('#casuals-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.5)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#casuals-text').css({
+            'font-size': '32px',
+            'transition': 'font-size 1s'
+        });
+    });
+
+    // ON MOUSE LEAVE
+    $('#casuals-holder').on('mouseleave',() => {
+        $('#casuals-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.2)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#causals-text').css({
+            'font-size': '30px',
+            'transition': 'font-size 1s'
+        });
+    })
+
+    // OTHERS
+
+    // ON MOUSE ENTER
+    $('#others-holder').on('mouseenter',() => {
+        $('#others-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.5)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#others-text').css({
+            'font-size': '32px',
+            'transition': 'font-size 1s'
+        });
+    });
+
+    // ON MOUSE LEAVE
+    $('#others-holder').on('mouseleave',() => {
+        $('#others-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.2)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#others-text').css({
+            'font-size': '30px',
+            'transition': 'font-size 1s'
+        });
+    })
+
+
+    // Kids
+
+    // ON MOUSE ENTER
+    $('#kids-holder').on('mouseenter',() => {
+        $('#kids-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.5)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#kids-text').css({
+            'font-size': '32px',
+            'transition': 'font-size 1s'
+        });
+    });
+
+    // ON MOUSE LEAVE
+    $('#kids-holder').on('mouseleave',() => {
+        $('#kids-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.2)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#kids-text').css({
+            'font-size': '30px',
+            'transition': 'font-size 1s'
+        });
+    })
+
+    // game
+
+    // ON MOUSE ENTER
+    $('#game-holder').on('mouseenter',() => {
+        $('#game-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.5)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#game-text').css({
+            'font-size': '32px',
+            'transition': 'font-size 1s'
+        });
+    });
+
+    // ON MOUSE LEAVE
+    $('#game-holder').on('mouseleave',() => {
+        $('#game-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.2)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#game-text').css({
+            'font-size': '30px',
+            'transition': 'font-size 1s'
+        });
+    })
+
+    // home
+
+    // ON MOUSE ENTER
+    $('#home-holder').on('mouseenter',() => {
+        $('#home-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.5)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#home-text').css({
+            'font-size': '32px',
+            'transition': 'font-size 1s'
+        });
+    });
+
+    // ON MOUSE LEAVE
+    $('#home-holder').on('mouseleave',() => {
+        $('#home-holder').css({
+            'background-color': 'rgba(0, 0, 0, 0.2)',
+            'transition': 'background-color 1s'
+        });
+
+        $('#home-text').css({
+            'font-size': '30px',
+            'transition': 'font-size 1s'
+        });
+    })
+})
